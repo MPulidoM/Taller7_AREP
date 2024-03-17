35a4db0e9af7711b7b0de8f3021f4977b150d0e9
MPulidoM <118181543+MPulidoM@users.noreply.github.com>
2024-03-17 17:06:00 -0500

    Documentación PasswordHasher.java

--- src/main/java/co/edu/escuelaing/arep/PasswordHasher.java
+++ src/main/java/co/edu/escuelaing/arep/PasswordHasher.java
@@ -2,8 +2,19 @@ package co.edu.escuelaing.arep;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-public class PasswordHasher {
 
+/**
+ * Clase para el hasheo de contraseñas utilizando el algoritmo SHA-3 de 256 bits.
+ */
+public class PasswordHasher {
+    
+     /**
+     * Hashea una contraseña utilizando el algoritmo SHA-3 de 256 bits.
+     *
+     * @param password La contraseña a hashear.
+     * @return El hash de la contraseña.
+     * @throws NoSuchAlgorithmException Si el algoritmo SHA-3 de 256 bits no está soportado.
+     */
     public static String HashPassword(String password) throws NoSuchAlgorithmException {
 
         MessageDigest md = MessageDigest.getInstance("SHA3-256");
@@ -17,8 +28,15 @@ public class PasswordHasher {
 
         return sb.toString();
     }
-
-
+    
+    /**
+     * Valida una contraseña hasheada contra un hash almacenado.
+     *
+     * @param passToValidate La contraseña a validar.
+     * @param correctPassword El hash almacenado de la contraseña correcta.
+     * @return Verdadero si la contraseña es válida, falso en caso contrario.
+     * @throws NoSuchAlgorithmException Si el algoritmo SHA-3 de 256 bits no está soportado.
+     */
     public static boolean validatePassword(String passToValidate, String correctPassword)
             throws NoSuchAlgorithmException {
         System.out.println(PasswordHasher.HashPassword(passToValidate));
